0c7b06325e95962307f7156e1ecabe3103848dfd
Bernát Gábor <gaborjbernat@gmail.com>
2026-01-27 14:39:12 -0800

    Add signing and publishing configuration for JetBrains Marketplace (#9)

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,14 @@ intellijPlatform {
             untilBuild = provider { null }
         }
     }
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
     pluginVerification {
         ides {
             recommended()
